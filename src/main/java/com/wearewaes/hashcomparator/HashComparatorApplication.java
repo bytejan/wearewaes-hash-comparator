3dc91716f63a8a5829ed76c3875c1ee2991899da
Jan-Jaap Arends <jj.arends@codeimpact.nl>
2020-07-02 03:08:02 +0200

    Initial commit, generated with spring initializr.

---
+++ src/main/java/com/wearewaes/hashcomparator/HashComparatorApplication.java
@@ -0,0 +1,13 @@
+package com.wearewaes.hashcomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashComparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashComparatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wearewaes/hashcomparator/HashComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wearewaes.hashcomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
