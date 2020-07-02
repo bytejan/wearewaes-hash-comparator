45358a964ff8e7d1ab66df063cf296e1127708d7
Jan-Jaap Arends <jj.arends@codeimpact.nl>
2020-07-02 15:55:26 +0200

    Add a docker compose file for the MySQL database.

--- src/main/java/com/wearewaes/hashcomparator/HashComparatorApplication.java
+++ src/main/java/com/wearewaes/hashcomparator/HashComparatorApplication.java
@@ -1,13 +1,9 @@
 package com.wearewaes.hashcomparator;
-
-import com.wearewaes.hashcomparator.service.HashCompareService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HashComparatorApplication {
-
-	private static HashCompareService hashCompareService = new HashCompareService();
 
 	public static void main(String[] args) {
 		SpringApplication.run(HashComparatorApplication.class, args);
